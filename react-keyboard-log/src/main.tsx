9b89da1d5bdd9227b9d55ad9bdec2fc3c3c0c5a3
Sebastian F <sebpfdev@gmail.com>
2022-10-11 23:25:29 -0400

    Move global styles to folder

--- react-keyboard-log/src/main.tsx
+++ react-keyboard-log/src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-import './index.css';
+import '~/styles/index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// prettier-ignore
+ReactDOM
+  .createRoot(document.getElementById('root') as HTMLElement)
+  .render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
